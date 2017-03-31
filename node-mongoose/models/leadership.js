d0832a8dbd104285ed5ed4c931df5f4130b92a96
TamerHussien <eng.tamerhussien@gmail.com>
2017-03-31 21:17:36 +0200

    Module-2 Assignment

---
+++ node-mongoose/models/leadership.js
@@ -0,0 +1,29 @@
+var mongoose = require('mongoose');
+
+var leaderSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  image: {
+    type: String,
+    required: true
+  },
+  designation: {
+    type: String,
+    required: true
+  },
+  abbr: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  }
+});
+
+var Leader = mongoose.model('Leader', leaderSchema);
+
+module.exports = Leader;
